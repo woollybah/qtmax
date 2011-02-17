4467635d81109a3fae89b9b6b8c5a25bc5bc7621
Brucey <woollybah@users.noreply.github.com>
2011-02-17 21:42:12 +0000

    Update to Qt 4.7.1

--- qt.mod/qtoolbar.mod/moc4glue.cpp
+++ qt.mod/qtoolbar.mod/moc4glue.cpp
@@ -1,8 +1,8 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'glue.h'
 **
-** Created: Tue 6. Oct 08:16:02 2009
-**      by: The Qt Meta Object Compiler version 61 (Qt 4.5.2)
+** Created: Wed Feb 16 20:25:39 2011
+**      by: The Qt Meta Object Compiler version 62 (Qt 4.7.1)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
@@ -10,8 +10,8 @@
 #include "glue.h"
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'glue.h' doesn't include <QObject>."
-#elif Q_MOC_OUTPUT_REVISION != 61
-#error "This file was generated using the moc from 4.5.2. It"
+#elif Q_MOC_OUTPUT_REVISION != 62
+#error "This file was generated using the moc from 4.7.1. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
@@ -20,13 +20,15 @@ QT_BEGIN_MOC_NAMESPACE
 static const uint qt_meta_data_MaxQToolBar[] = {
 
  // content:
-       2,       // revision
+       5,       // revision
        0,       // classname
        0,    0, // classinfo
-       7,   12, // methods
+       7,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
 
  // slots: signature, parameters, type, tag, flags
       20,   13,   12,   12, 0x08,
@@ -56,9 +58,13 @@ const QMetaObject MaxQToolBar::staticMetaObject = {
       qt_meta_data_MaxQToolBar, 0 }
 };
 
+#ifdef Q_NO_DATA_RELOCATION
+const QMetaObject &MaxQToolBar::getStaticMetaObject() { return staticMetaObject; }
+#endif //Q_NO_DATA_RELOCATION
+
 const QMetaObject *MaxQToolBar::metaObject() const
 {
-    return &staticMetaObject;
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->metaObject : &staticMetaObject;
 }
 
 void *MaxQToolBar::qt_metacast(const char *_clname)
