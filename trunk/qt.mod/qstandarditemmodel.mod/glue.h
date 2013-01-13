57b4970c1a2e1a5bf61661c619aa758a71546027
woollybah <woollybah@5d3d2ab8-7492-11de-9510-cf8a13bd668e>
2013-01-13 01:45:47 +0000

    Implemented hasChildren() on QStandardItemModel. QtMaxGUI. Added menu item state effects.

--- trunk/qt.mod/qstandarditemmodel.mod/glue.h
+++ trunk/qt.mod/qstandarditemmodel.mod/glue.h
@@ -109,6 +109,7 @@ public:
 	virtual bool setData(const QModelIndex & index, const QVariant & value, int role = Qt::EditRole);
 	virtual bool insertRows(int row, int count, const QModelIndex & parent = QModelIndex());
 	virtual bool removeRows(int row, int count, const QModelIndex & parent = QModelIndex());
+	virtual bool hasChildren(const QModelIndex & parent = QModelIndex()) const;
 
 	QModelIndex doCreateIndex(int row, int col, MaxQStandardItem * parent);
 	void itemChanged(const QModelIndex & index1, const QModelIndex & index2);
